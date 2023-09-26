35e922769918fe4381d7628b2c700a78925d96d8
mulchus <mulch@list.ru>
2023-09-26 23:34:41 +0300

    Updated settings to deploy

--- django_foodplan/settings.py
+++ django_foodplan/settings.py
@@ -89,6 +89,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
