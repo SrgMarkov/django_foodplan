4d5e9ce5dc917c921bae144aaf4a0d5bbe696ea7
SrgMarkov <srg.markov85@gmail.com>
2023-09-28 12:15:34 +0400

    Add login

--- foodplan/views.py
+++ foodplan/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+import random
+
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import *
+from .models import Rate, Ingredient, Recipe, RecipeItem, Order
 
 
 class RegisterUser(CreateView):
@@ -15,7 +20,61 @@ class RegisterUser(CreateView):
         return dict(list(context.items()))
 
 
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'auth.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('lk')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+
+
+def free_recipes(request):
+    recipes = []
+    try:
+        random_breakfast = random.sample(list(Recipe.objects.filter(meal_time='завтрак')), k=1)[0]
+        recipes.append(random_breakfast)
+    except ValueError:
+        pass
+    try:
+        random_lunch = random.sample(list(Recipe.objects.filter(meal_time='обед')), k=1)[0]
+        recipes.append(random_lunch)
+    except ValueError:
+        pass
+    try:
+        random_dinner = random.sample(list(Recipe.objects.filter(meal_time='ужин')), k=1)[0]
+        recipes.append(random_dinner)
+    except ValueError:
+        pass
+    try:
+        random_dessert = random.sample(list(Recipe.objects.filter(meal_time='десерт')), k=1)[0]
+        recipes.append(random_dessert)
+    except ValueError:
+        pass
+
+    # selected_bouquets = list(Bouquet.objects.all())
+    # random_selected_bouquets = random.sample(selected_bouquets, k=3)
+    # return render(request, 'index.html', {'bouquets': random_selected_bouquets})
+
+    # context = {
+    #     'random_breakfast': random_breakfast,
+    #     'random_lunch': random_lunch,
+    #     'random_dinner': random_dinner,
+    #     'random_dessert': random_dessert,
+    # }
+    return render(request, 'free_recipes.html', {'recipes': recipes})
+
+
 def index(request):
+    request.session.clear()
     return render(request, 'index.html')
 
 
