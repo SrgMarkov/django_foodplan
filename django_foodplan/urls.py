25d6bd6d360a54e83b9db98ac2b126dfa37978ef
SrgMarkov <srg.markov85@gmail.com>
2023-09-28 08:16:13 +0400

    Add login, extand base

--- django_foodplan/urls.py
+++ django_foodplan/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('order/', order, name='order'),
     path('card/', card, name='card'),
-    path('auth/', auth, name='auth'),
+    path('auth/', LoginUser.as_view(), name='auth'),
     path('registration/', RegisterUser.as_view(), name='registration'),
+    path('logout/', logout_user, name='logout'),
     path('lk/', lk, name='lk'),
 ]
--- foodplan/views.py
+++ foodplan/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -15,16 +17,25 @@ class RegisterUser(CreateView):
         return dict(list(context.items()))
 
 
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'auth.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('lk')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
+
+
 def index(request):
     return render(request, 'index.html')
-
-
-def auth(request):
-    return render(request, 'auth.html')
-
-
-def registration(request):
-    return render(request, 'registration.html')
 
 
 def lk(request):
